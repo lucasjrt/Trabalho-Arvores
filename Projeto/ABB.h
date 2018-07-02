385bf2cbbdb56c4ef61d0e4577dca04c976be415
cizinho10 <tarcisioaraujo123@hotmail.com>
2018-07-01 22:16:07 -0300

    Add files via upload

--- Projeto/ABB.h
+++ Projeto/ABB.h
@@ -1,33 +1,38 @@
-#ifndef ABB_H_INCLUDED
-#define ABB_H_INCLUDED
-
-typedef struct dados* Aluno;
-typedef struct no* Arvore;
-
-Arvore cria_vazia(); //Cria uma Arvore vazia
-Arvore cria_arvore(Aluno a, Arvore sae, Arvore sad); //Cria uma arvore
-int arvore_vazia(Arvore a); //Verifica se uma Arvore e vazia
-void libera_arvore(Arvore *a); //Desaloca cada espaço da arvore
-int existe(Arvore a, Aluno elem);
-int remove_folha(Arvore *a, Aluno aluno);
-void print_arvore(Arvore a);
 
 
-Aluno maior(Arvore a); // Aluno mais velho
-int de_maior(Arvore a); // Alunos idade > 18
-int qtde_alunos(Arvore a, int ini, int fim); // Idade entre ini e fim
-int um_filho(Arvore a); // Qtd nó da ABB que possuem apenas 1 filho;
-int completa (Arvore a); // Verifica se a ABB é completa
-Arvore juntar (Arvore a1, Arvore a2); // Retorna uma Arvore que é a intercalação de a1 e a2
-char *get_nome(Aluno a);
-int get_idade(Aluno a);
-char *get_curso(Aluno a);
-void set_nome(Aluno a, char *nome);
-void set_idade(Aluno a, int idade);
-void set_curso(Aluno a, char *curso);
-int bytes_struct_dados();
-Arvore *get_sae_add(Arvore a);
-Arvore *get_sad_add(Arvore a);
-Aluno get_aluno(Arvore a);
+typedef struct reg Estrutura;
+typedef struct reg *Reg;
+typedef struct no No;
+typedef struct no *Arv;
 
-#endif // ABB_H_INCLUDED
+Arv cria_arvore();
+int arvore_vazia(Arv *arv);
+int libera_arvore(Arv *arv);
+void exibe_arvore(Arv *a);
+void exibe_arvore_por_nome(Arv *a);
+void exibe_ordenado(Arv *a);
+void exibe_ordenado_por_nome(Arv *a);
+void mostrar_registro(Reg r);
+No *criar_no();
+No *criar_no_temp(char *nome,char *curso,int idade);
+int insere_copia_ord(Arv *a,No *no);
+int insere_ord(Arv *a,No *no);
+int remove_ord(Arv *a,int idade);
+void imprimir_arvore(Arv a);
+Reg maior(Arv a);
+void faixaIdade(Arv a,int *quantidade,int min,int max);
+int de_maior(Arv a);
+int qtde_alunos(Arv a,int ini,int fim);
+void um_filho_recursiva(Arv a,int *quantidade);
+int um_filho(Arv a);
+int altura_arv(Arv a);
+void altura_arv_temp(Arv a,int nivel,int *max);
+int completa(Arv a);
+int completa_temp(Arv a,Arv a_raiz,int nivel_raiz);
+int profundidade_no(Arv a,Reg r);
+int profundidade_no_temp(Arv a,Reg r,int nivel);
+No *remove_menor(Arv *a);
+void juntar_temp(Arv a1,Arv a2,Arv *a3);
+Arv juntar(Arv a1,Arv a2);
+void mostra_no(No *no);
+
